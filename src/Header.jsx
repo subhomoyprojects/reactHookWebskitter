450b7f6821c8ad99f8d5a1cac611d4578f990c8e
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-08-07 22:40:37 +0530

    try to tab

--- src/Header.jsx
+++ src/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/tab">
+                <Link className="nav-link" to="/">
                   Tab
                 </Link>
               </li>
