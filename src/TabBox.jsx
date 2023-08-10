3cca3d32765c7be7368d70b1e2ae2da75f5ccbe4
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-08-11 00:03:23 +0530

    add tab

--- src/TabBox.jsx
+++ src/TabBox.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import { useState } from "react";
+import { Chow1, Noodles, Biryani } from "./images/Images";
 import "./TabBox.css";
 export default function TabBox() {
+  const [tabShow, setTabShow] = useState(0);
+
+  const showHideTabBody = (event) => {
+    if (event.target.classList.contains("chowmein")) {
+      setTabShow(0);
+    } else if (event.target.classList.contains("noodle")) {
+      setTabShow(1);
+    } else if (event.target.classList.contains("biryani")) {
+      setTabShow(2);
+    }
+  };
   return (
     <>
       <main className="main">
@@ -10,26 +22,70 @@ export default function TabBox() {
               <div className="tabBox">
                 <ul className="tabBox-header">
                   <li>
-                    <button type="button" className="active">
+                    <button type="button" onClick={showHideTabBody} className="chowmein">
                       Chowmein
                     </button>
                   </li>
                   <li>
-                    <button type="button">Noodle</button>
+                    <button type="button" onClick={showHideTabBody} className="noodle">
+                      Noodle
+                    </button>
                   </li>
                   <li>
-                    <button type="button">Biryani</button>
+                    <button type="button" onClick={showHideTabBody} className="biryani">
+                      Biryani
+                    </button>
                   </li>
                 </ul>
                 <div className="tab-body">
-                  <div className="item chow-mein">
+                  <div className={"item chowMein" + (tabShow === 0 ? " show" : "")}>
                     <h3>Chow mein</h3>
-                    <figure className="chow-mein-img"></figure>
+                    <figure className="img-holder">
+                      <img src={Chow1} alt="" />
+                    </figure>
                     <p>
                       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste modi ratione aspernatur, praesentium, expedita mollitia nobis molestias, natus voluptate obcaecati tempora facilis soluta asperiores error debitis labore accusamus
                       laudantium id?
                     </p>
+                    <h4>Ingredient</h4>
                     <ul className="ingredient">
+                      <li>Lorem Ipsum</li>
+                      <li>Lorem Ipsum</li>
+                      <li>Lorem Ipsum</li>
+                      <li>Lorem Ipsum</li>
+                    </ul>
+                  </div>
+                  <div className={"item noodle" + (tabShow === 1 ? " show" : "")}>
+                    <h3>Noodles</h3>
+                    <figure className="img-holder">
+                      <img src={Noodles} alt="" />
+                    </figure>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste modi ratione aspernatur, praesentium, expedita mollitia nobis molestias, natus voluptate obcaecati tempora facilis soluta asperiores error debitis labore accusamus
+                      laudantium id?
+                    </p>
+                    <h4>Ingredient</h4>
+                    <ul className="ingredient">
+                      <li>Lorem Ipsum</li>
+                      <li>Lorem Ipsum</li>
+                      <li>Lorem Ipsum</li>
+                      <li>Lorem Ipsum</li>
+                    </ul>
+                  </div>
+                  <div className={"item biryani" + (tabShow === 2 ? " show" : "")}>
+                    <h3>Biryani</h3>
+                    <figure className="img-holder">
+                      <img src={Biryani} alt="" />
+                    </figure>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste modi ratione aspernatur, praesentium, expedita mollitia nobis molestias, natus voluptate obcaecati tempora facilis soluta asperiores error debitis labore accusamus
+                      laudantium id?
+                    </p>
+                    <h4>Ingredient</h4>
+                    <ul className="ingredient">
+                      <li>Lorem Ipsum</li>
+                      <li>Lorem Ipsum</li>
+                      <li>Lorem Ipsum</li>
                       <li>Lorem Ipsum</li>
                     </ul>
                   </div>
---
+++ src/images/Images.jsx
@@ -0,0 +1,5 @@
+import Chow1 from "./chow1.png";
+import Noodles from "./chow2.png";
+import Biryani from "./biryani.png";
+
+export { Chow1, Noodles, Biryani };
